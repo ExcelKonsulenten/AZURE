1a4edbe199d01f0e4d7fa028e0650d220fd25efc
ExcelKonsulenten <prosjekt@excelkonsulenten.no>
2025-10-20 12:21:44 +0200

    Delete visma_to_mysql directory

--- visma_to_mysql/__init__.py
+++
@@ -1,9 +0,0 @@
-import datetime
-import logging
-
-import azure.functions as func
-
-def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().replace(
-        tzinfo=datetime.timezone.utc).isoformat()
-    logging.info(f"Python timer trigger function ran at {utc_timestamp}")
