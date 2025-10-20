7965e7a738836b1a646dbea157cb312b7a5507cc
ExcelKonsulenten <prosjekt@excelkonsulenten.no>
2025-10-20 12:15:26 +0200

    Create __init__.py

---
+++ visma_to_mysql/__init__.py
@@ -0,0 +1,9 @@
+import datetime
+import logging
+
+import azure.functions as func
+
+def main(mytimer: func.TimerRequest) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+    logging.info(f"Python timer trigger function ran at {utc_timestamp}")
